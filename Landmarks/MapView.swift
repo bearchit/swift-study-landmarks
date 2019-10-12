55af7d957159acdba250752a782795214dbd491c
bearchit <bearchit@gmail.com>
2019-10-12 22:40:30 +0900

    Building Lists and Navigation

--- Landmarks/MapView.swift
+++ Landmarks/MapView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 import MapKit
 
 struct MapView: UIViewRepresentable {
+    var coordinate: CLLocationCoordinate2D
+    
     func makeUIView(context: Context) -> MKMapView {
         MKMapView(frame: .zero)
     }
@@ -18,7 +20,7 @@ struct MapView: UIViewRepresentable {
         let coordinate = CLLocationCoordinate2D(
             latitude: 34.011286, longitude: -116.166868
         )
-        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
         let region = MKCoordinateRegion(center: coordinate, span: span)
         view.setRegion(region, animated: true)
     }
@@ -26,7 +28,7 @@ struct MapView: UIViewRepresentable {
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(coordinate: landmarkData[0].locationCoordinate)
     }
 }
         
