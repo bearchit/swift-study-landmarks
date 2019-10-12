860b5b0d45d47aa4da40857ea4e621c2bfa9d3f7
bearchit <bearchit@gmail.com>
2019-10-12 19:41:21 +0900

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Kim Mingoo on 2019/10/12.
+//  Copyright © 2019 Kim Mingoo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
